547a58eebb77d90a1309504ff80ef288af57ebb7
Pavlan868 <126884281+Pavlan868@users.noreply.github.com>
2023-11-18 11:08:34 +0700

    Add files via upload

--- unit_test.cpp
+++ unit_test.cpp
@@ -6,9 +6,11 @@
 #include <sstream>
 #include <vector>
 
-#include "ConsoleApplication12/CarInfo.h"
-#include "ConsoleApplication12/reader.h"
-#include "ConsoleApplication12/Date.h"
+//C:\Users\я\source\repos\ConsoleApplication32\ConsoleApplication32\pract2\OPPO\OPPO\LAB3\ConsoleApplication12
+
+#include "../../LAB3/ConsoleApplication12/CarInfo.h"
+#include "../../LAB3/ConsoleApplication12/reader.h"
+#include "../../LAB3/ConsoleApplication12/Date.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -45,31 +47,129 @@ namespace TestProject
 
         TEST_METHOD(ValidCarNumber)
         {
-            CarInfo car;
+            Assert::IsTrue(CarInfo::isValidCarNumber("A123BC"));
+            Assert::IsTrue(CarInfo::isValidCarNumber("D530FY"));
+            Assert::IsTrue(CarInfo::isValidCarNumber("U408TY"));
+            Assert::IsTrue(CarInfo::isValidCarNumber("G441RT"));
+            Assert::IsTrue(CarInfo::isValidCarNumber("F862KL"));
+            Assert::IsTrue(CarInfo::isValidCarNumber("A132CB"));
+            Assert::IsTrue(CarInfo::isValidCarNumber("R541KL"));
+            Assert::IsTrue(CarInfo::isValidCarNumber("P842OL"));
+            Assert::IsTrue(CarInfo::isValidCarNumber("T011EW"));
 
-            // Проверка валидного номера
-            car.carNumber = "A123BC";
-            Assert::IsTrue(car.isValidCarNumber(car.carNumber));
-            if (car.isValidCarNumber(car.carNumber)) {
-                Microsoft::VisualStudio::CppUnitTestFramework::Logger::WriteMessage("ValidateCarNumber: Success\n");
+        }
+
+// CarInfo
+// -----------------------------------------------------------------------
+
+        TEST_METHOD(CarInfo_readCarInfoFromFile_err)
+        {
+            std::vector<std::string> cases{
+               "12456",
+               "4dfsfwegxf",
+               "l;l;;;pp;"
+            };
+
+            for (const auto& testCase : cases) {
+                try {
+                    std::stringstream sin{ testCase };
+                    CarInfo::readCarInfoFromFile(sin);
+                    Assert::Fail();
+                }
+                catch (const std::runtime_error& e) {
+                    std::string message{"Expected err: " + std::string(e.what())};
+                    Logger::WriteMessage(message.c_str());
+                }
+                catch (...) {
+                    Assert::Fail();
+                }
+            }
+        }
+
+        TEST_METHOD(CarInfo_readCarInfoFromFile_ok)
+        {
+            std::vector<std::string> cases{
+                "11.11.2023 A123BC",
+
+            };
+
+            for (const auto& testCase : cases) {
+                try {
+                    std::stringstream sin{ testCase };
+                    CarInfo::readCarInfoFromFile(sin);
+                }
+                catch (...) {
+                    Logger::WriteMessage(testCase.c_str());
+                    Assert::Fail();
+                }
+            }
+        }
+
+// Date
+// -----------------------------------------------------------------------
+
+        // TODO:
+        TEST_METHOD(Date_create_from_stream_err)
+        {
+            //Assert::ExpectException<std::runtime_error>()
+
+            std::vector<std::string> cases{
+                "12432414",
+                "cvdcdedsc",
+                ".",
+                "124r56th5h",
+
+            };
+
+            for (const auto& testCase : cases) {
+                try {
+                    std::stringstream sin{ testCase };
+                    Date::create_from_stream(sin);
+                    Assert::Fail();
+                }
+                catch (const std::runtime_error& e) {
+                    std::string message{ "Expected err: " + std::string(e.what()) };
+                    Logger::WriteMessage(message.c_str());
+                }
+                catch (...) {
+                    Assert::Fail();
+                }
             }
 
-            Assert::IsTrue(car.isValidCarNumber("A123BC"));
-            Assert::IsTrue(car.isValidCarNumber("D530FY"));
-            Assert::IsTrue(car.isValidCarNumber("U408TY"));
-            Assert::IsTrue(car.isValidCarNumber("G441RT"));
-            Assert::IsTrue(car.isValidCarNumber("F862KL"));
-            Assert::IsTrue(car.isValidCarNumber("A132CB"));
-            Assert::IsTrue(car.isValidCarNumber("R541KL"));
-            Assert::IsTrue(car.isValidCarNumber("P842OL"));
-            Assert::IsTrue(car.isValidCarNumber("T011EW"));
+        }
 
-            // TODO: добавить несколько случаев (OK)
+        // TODO:
 
+        TEST_METHOD(Date_create_from_stream_ok)
+        {
+            std::vector<std::string> cases{
+                "13.12.2003 D530FY",
+                "21.08.2003 D530FY",
+                "15.02.2004 D530FY",
+                "04.09.2013 D530FY",
+            };
+
+            for (const auto& testCase : cases) {
+                try {
+                    std::istringstream iss(testCase);
+                    std::string date;
+                    iss >> date;  // Читаем только дату из строки
+                    std::stringstream sin(date);  // Создаем новый поток только для даты
+                    CarInfo car;
+                    car.readCarInfoFromFile(sin);
+                }
+                catch (...) {
+                    Assert::IsTrue(L"Exception was thrown unexpectedly.");
+                }
+            }
+
+            // Если выполнение цикла завершилось без исключений, отмечаем тест как пройденный
+            Assert::IsTrue(true, L"All cases passed successfully.");
         }
 
         TEST_METHOD(InvalidCarNumber)
         {
+
             // TODO: перенести в отдельную группу  ---> InvalidCarNumber (OK)
             // Проверка невалидных номеров
             CarInfo car;
@@ -112,40 +212,31 @@ namespace TestProject
         TEST_METHOD(TestReadCarInfo)
         {
             std::vector<CarInfo> carList;
+            std::stringstream sin{
+                "13.12.2003 D530FY\n"
+                "21.08.2003 D530FY\n"
+                "15.02.2004 D530FY\n"
+                "04.09.2013 D530FY"
+            };
 
-            std::ifstream inputFile("D:\\avtomobili.txt");
-            if (!inputFile.is_open()) {
-                std::cerr << "Error opening the file." << std::endl;
-                Microsoft::VisualStudio::CppUnitTestFramework::Logger::WriteMessage("Error opening the file.");
+            readCarInfo(sin, carList);
 
-                exit(1);
-            }
-            readCarInfo(inputFile, carList);
-
-                // Assuming you have a specific file with known data, you can check if carList is as expected.
-            if (carList.size() > 0) {
-                // Ensure carList is not empty
-                Assert::IsTrue(true, L"Car list is not empty.");
-
-                if (!carList.empty()) {
-                    // Check the first car's carNumber
-                    Assert::AreEqual("D530FY", carList[0].carNumber.c_str(), L"First car's carNumber is not as expected.");
-                    //Microsoft::VisualStudio::CppUnitTestFramework::Logger::WriteMessage("D530FY, First car's carNumber is not as expected.");
-                    if (!carList.empty()) {
-                        Microsoft::VisualStudio::CppUnitTestFramework::Logger::WriteMessage("InvalidDate1Test: Success\n");
-                    }
-                    else {
-                        Microsoft::VisualStudio::CppUnitTestFramework::Logger::WriteMessage("InvalidDate1Test: Failure");
-                    }
+            // Ensure carList has exactly 4 elements
+            Assert::AreEqual(size_t(4), carList.size(), L"Car list size is not as expected.");
+       /*     Assert::IsTrue(carList.size() == 4, L"Car list size is not as expected.");*/
 
 
-                }
+            if (!carList.empty()) {
+                // Check the first car's carNumber
+                //Assert::IsTrue(carList[0].carNumber == "D530FY", L"First car's carNumber is not as expected.");
+
+                Assert::AreEqual("D530FY", carList[0].carNumber.c_str(), L"First car's carNumber is not as expected.");
+                Logger::WriteMessage("TestReadCarInfo: ✓ Success\n");
             }
             else {
                 // Output an error message if the test failed
-                Microsoft::VisualStudio::CppUnitTestFramework::Logger::WriteMessage("TestReadCarInfo failed: Car list is empty.");
+                Logger::WriteMessage("TestReadCarInfo failed: Car list is empty.\n");
             }
-
         }
     };
 
@@ -159,8 +250,7 @@ namespace TestProject
             Assert::IsTrue(Date{ 29, 02, 2024 }.isValid());
             Assert::IsTrue(Date{ 28, 02, 2023 }.isValid());
             Assert::IsTrue(Date{ 05, 07, 2005 }.isValid());
-            // TODO: добавить несколько случаев (OK)
-            //...
+
             Assert::IsTrue(Date{ 29, 02, 1992 }.isValid());
             Assert::IsTrue(Date{ 29, 02, 1996 }.isValid());
             Assert::IsTrue(Date{ 29, 02, 2000 }.isValid());
@@ -224,7 +314,7 @@ namespace TestProject
             Assert::IsFalse(Date{ -1, 07, 2005 }.isValid());
             Assert::IsFalse(Date{ 0, 06, 2022 }.isValid());
             Assert::IsFalse(Date{ -1, 07, 2005 }.isValid());
-            // TODO: добавить несколько случаев (OK)
+         
             Assert::IsFalse(Date{ 29, 02, 2023 }.isValid());
             Assert::IsFalse(Date{ 29, 02, 20017 }.isValid());
             Assert::IsFalse(Date{ 29, 02, 2005 }.isValid());
